96be66e434da84fe59d2946cacd45fbfe169fc09
moonyango <mosesadrian825@gmail.com>
2024-04-26 15:31:14 +0300

    two sums

---
+++ twoSums/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func twoSum(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return []int{}
+}
+
+func main() {
+	// Test cases
+	nums := []int{2, 7, 11, 15}
+	target := 9
+	result := twoSum(nums, target)
+	fmt.Println(result) // Output: [0 1]
+
+	nums = []int{3, 2, 4}
+	target = 6
+	result = twoSum(nums, target)
+	fmt.Println(result) // Output: [1 2]
+
+	nums = []int{3, 3}
+	target = 6
+	result = twoSum(nums, target)
+	fmt.Println(result) // Output: [0 1]
+}
