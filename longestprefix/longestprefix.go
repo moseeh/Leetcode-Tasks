7c2ff482e7c272d6c8744d3eb61dc64637d53b82
moonyango moonyango <moseeh>
2024-06-03 17:30:19 +0300

    longest word in go

---
+++ longestprefix/longestprefix.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println(longestCommonPrefix([]string{"hello", "hellwowow", "helloworld"}))
+}
+
+func longestCommonPrefix(strs []string) string {
+	prefix := strs[0]
+	for i := 1; i < len(strs); i++ {
+		for !strings.HasPrefix(strs[i], prefix) {
+			prefix = prefix[:len(prefix)-1]
+			if prefix == "" {
+				return ""
+			}
+		}
+	}
+	return prefix
+}
